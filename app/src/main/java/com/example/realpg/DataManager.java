d65f6b7205423bd1727a0ef01b356d4a3f59dcc5
Eros Guerrero <94256335+erosguerrero@users.noreply.github.com>
2023-04-22 11:08:53 +0200

    Ejemplos en DM y cambios en Evolution

--- app/src/main/java/com/example/realpg/DataManager.java
+++ app/src/main/java/com/example/realpg/DataManager.java
@@ -86,6 +86,51 @@ public class DataManager {
         }
     }
 
+    public void demoExtraJson(){
+        JSONObject extraJson = new JSONObject();
+        try {
+            extraJson.put("Length", 10);
+            JSONObject last3json = new JSONObject();
+            JSONObject oneof3json = new JSONObject();
+            oneof3json.put("ID", 9);
+            oneof3json.put("nombre", "Testeoo");
+            last3json.put("1",oneof3json);             last3json.put("2",oneof3json);
+            last3json.put("3",oneof3json);
+            extraJson.put("Last3", last3json);
+
+            JSONObject pokechosen = new JSONObject();
+            pokechosen.put("IdEvolucion", 0);
+            pokechosen.put("xp", 15.5);
+            extraJson.put("PokeChosen",pokechosen);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        this.save( "extra.json",extraJson);
+    }
+
+    public void demoPokemonJson(){
+        JSONObject pokemonJson = null;
+        try {
+            pokemonJson = new JSONObject("{\n" +
+                    "\t\"0\": {\n" +
+                    "\t\t\"level\": \"14,7\",\n" +
+                    "\t\t\"names\": [\"pichu\", \"pikachu\", \"raichu\"],\n" +
+                    "\t\t\"ids\": [14, 15, 16],\n" +
+                    "\t\t\"levels\": [12, 25]\n" +
+                    "\t},\n" +
+                    "\t\"1\": {\n" +
+                    "\t\t\"level\": \"22,3\",\n" +
+                    "\t\t\"names\": [\"chimchar\", \"monferno\", \"infernape\"],\n" +
+                    "\t\t\"ids\": [502, 503, 504],\n" +
+                    "\t\t\"levels\":[16,35]\n" +
+                    "\t}\n" +
+                    "}");
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+        this.save( "pokemon.json",pokemonJson);
+    }
     public static void demoCreateActivitiesData(Context context)
     {
         String demodata= "{\n" +
--- app/src/main/java/com/example/realpg/Evolution.java
+++ app/src/main/java/com/example/realpg/Evolution.java
@@ -1,5 +1,9 @@
 package com.example.realpg;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,11 +12,21 @@ public class Evolution {
     private List<Integer> ids;
     private ArrayList<Integer> levels;
     private List<String> names;
+    private double currentXp;
     private int current;
 
     public Evolution(){
         id = null;
         init();
+    }
+
+    public Evolution(Integer id, List<Integer> ids, ArrayList<Integer> levels, List<String> names, double currentXp, int current) {
+        this.id = id;
+        this.ids = ids;
+        this.levels = levels;
+        this.names = names;
+        this.currentXp = currentXp;
+        this.current = current;
     }
 
     public Evolution(Integer id){
@@ -33,6 +47,45 @@ public class Evolution {
         names.add(name);
     }
 
+    public JSONObject toJson()
+    {
+        JSONObject json = new JSONObject();
+        try {
+            JSONObject jsonData = new JSONObject();
+            jsonData.put("level", this.currentXp);
+            jsonData.put("levels", this.levels);
+            jsonData.put("names", names);
+            jsonData.put("ids", this.ids);
+            json.put(String.valueOf(this.id),jsonData);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        return json;
+    }
+    /**Genera un objeto nuevo de tipo Evolution de un JSON
+     *
+     * @param idEvolution Id de la cadena evolutiva
+     * @param json el jsonData de la id en cuestión
+     * @return
+     */
+    public static Evolution createEvolutionFromJson(int idEvolution, JSONObject json)
+    {
+        try {
+            double xpCurrent = json.getDouble("level");
+            //levels
+            //names
+            //ids
+
+
+            return new Evolution();
+
+
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
     public Integer getId() {
         return id;
     }
