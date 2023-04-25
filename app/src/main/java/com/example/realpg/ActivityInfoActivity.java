609133657e66828dddbc8d42e8e409117ea84e4f
AndresRomero2001 <androm10@ucm.es>
2023-04-25 19:34:52 +0200

    Revert "top activities dinamico"
    This reverts commit 210777a5ed25af08f160af2a646f8295e013247d.

--- app/src/main/java/com/example/realpg/ActivityInfoActivity.java
+++ app/src/main/java/com/example/realpg/ActivityInfoActivity.java
@@ -38,6 +38,76 @@ public class ActivityInfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
 
+
+        //Leer datos del json almacenado en disco
+        //simulados ahora con el string readedData
+
+
+        /*String readedData = "\n" +
+                "{\"activities\":[{\"idAct\":0,\"totalMin\":50,\"latestSessions\":[{\"date\":\"2023-04-04\",\"minutes\":20},{\"date\":\"2023-04-17\",\"minutes\":15}]},\n" +
+                "\t       {\"idAct\":1,\"totalMin\":12,\"latestSessions\":[{\"date\":\"2023-03-03\",\"minutes\":20},{\"date\":\"2023-03-22\",\"minutes\":2}]}]}";
+*/
+        //data como json completo, sin jsonArray
+        //TODO quizas no necesario primera clave activities si este json esta en un fichero diferente a los demas
+       //ahora mismo esta hecho como si esuviera en otro archivo diferente a lso demas
+        //TODO ver si necesario guardra un valor en el json que sea siguiente id
+        //se utilizaria como id al crear una actividad y se incrementaria en uno
+        //Asi asegura ids unicas sin mucha complejidad
+        /*String readedData = "{\n" +
+                "\n" +
+                "\t\"0\": {\n" +
+                "\t\t\"cat\": \"CASA\",\n" +
+                "\t\t\"name\": \"Actividad 0\",\n" +
+                "\t\t\"totalMin\": 50,\n" +
+                "\t\t\"latestSessions\": [{\n" +
+                "\t\t\t\"date\": \"2023-04-04\",\n" +
+                "\t\t\t\"minutes\": 20\n" +
+                "\t\t}, {\n" +
+                "\t\t\t\"date\": \"2023-04-17\",\n" +
+                "\t\t\t\"minutes\": 15\n" +
+                "\t\t}]\n" +
+                "\t},\n" +
+                "\t\"1\": {\n" +
+                "\t\t\"cat\": \"BIENESTAR\",\n" +
+                "\t\t\"name\": \"Actividad 1\",\n" +
+                "\t\t\"totalMin\": 60,\n" +
+                "\t\t\"latestSessions\": [{\n" +
+                "\t\t\t\"date\": \"2023-04-04\",\n" +
+                "\t\t\t\"minutes\": 20\n" +
+                "\t\t}, {\n" +
+                "\t\t\t\"date\": \"2023-04-17\",\n" +
+                "\t\t\t\"minutes\": 15\n" +
+                "\t\t}]\n" +
+                "\t},\n" +
+                "\t\"2\": {\n" +
+                "\t\t\"cat\": \"BIENESTAR\",\n" +
+                "\t\t\"name\": \"Actividad 2\",\n" +
+                "\t\t\"totalMin\": 20,\n" +
+                "\t\t\"latestSessions\": [{\n" +
+                "\t\t\t\"date\": \"2023-04-04\",\n" +
+                "\t\t\t\"minutes\": 20\n" +
+                "\t\t}, {\n" +
+                "\t\t\t\"date\": \"2023-04-17\",\n" +
+                "\t\t\t\"minutes\": 15\n" +
+                "\t\t}]\n" +
+                "\t},\n" +
+                "\t\"3\": {\n" +
+                "\t\t\"cat\": \"ESTUDIOS\",\n" +
+                "\t\t\"name\": \"Actividad 3\",\n" +
+                "\t\t\"totalMin\": 10,\n" +
+                "\t\t\"latestSessions\": [{\n" +
+                "\t\t\t\"date\": \"2023-04-04\",\n" +
+                "\t\t\t\"minutes\": 20\n" +
+                "\t\t}, {\n" +
+                "\t\t\t\"date\": \"2023-04-17\",\n" +
+                "\t\t\t\"minutes\": 15\n" +
+                "\t\t}]\n" +
+                "\t}\n" +
+                "\n" +
+                "}";*/
+
+
+
         try {
             DataManager dm = new DataManager(this);
             //JSONObject json = new JSONObject(readedData);
@@ -66,6 +136,24 @@ public class ActivityInfoActivity extends AppCompatActivity {
             timeMonth.setText(ac.getMinutesLatestDaysFormatted(30));
             timeYear.setText(ac.getMinutesLatestDaysFormatted(365));
             timeTotal.setText(ac.getFormattedTimeTotal());
+
+
+
+        /*    JSONArray jsonArrayAct = new JSONArray();
+
+            jsonArrayAct = json.getJSONArray("activities");
+            Log.i("demo", "JsonArray leido: "+ jsonArrayAct);
+             //JSONObject jsonAct = jsonArrayAct.getJSONObject(0);
+            */
+/*Todo: el index coincide con el id. para que esto sea asi cuando una actividad se borre seguira
+estando en el json pero se debera poner con info vacia para que no ocupe espacio innecesariamente
+Sino habria que ver como guardar las actividades referenciandolas por su id
+Podria ser un Json actividades que contiene claves idActividad y sus valores es toda la info de esa actividad
+
+*/
+            //TODO Ahora mismo lo anterior no es necesario, ya que se accede por ids
+
+
 
             categoriesSpinner = findViewById(R.id.categorySpinner);
 
@@ -207,7 +295,9 @@ public class ActivityInfoActivity extends AppCompatActivity {
             throw new RuntimeException(e);
         }
 
+
     }
+
 
     private ArrayList<String> manageSpinnerList(String firstCategory){
         String[] categoriesList = {"Casa", "Deporte", "Estudios", "Ocio", "Proyectos", "Trabajo", "Viajes", "Otros"};
